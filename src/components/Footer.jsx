616e6005827ccd289fe38ccb002a3cfff08249d0
Anyidollar <ifeanyionyekwere247@gmail.com>
2024-07-02 08:45:04 +0100

    completed the button component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,3 @@
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Frame from "../assets/Frame 01.svg";
 import Button from "./Button";
 import { LiaFacebook, LiaTwitter, LiaInstagram } from "react-icons/lia";
